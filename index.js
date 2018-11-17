38693f9a02a188f2103e0c7ef23049cdaa7c76df
mRcfps <1402491442@qq.com>
2018-11-17 15:41:56 +0800

    Init project

---
+++ index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const opn = require('opn');
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+const publicPath = path.join(__dirname, 'public');
+
+app.use(express.static(publicPath));
+
+app.get('/', (req, res) => {
+  res.send(fs.readFileSync(path.join(publicPath, 'index.html')).toString());
+});
+
+app.listen(port, () => {
+  const url = `http://localhost:${port}`;
+  console.log(`MILL is listening on ${url}!`);
+  opn(url);
+});
